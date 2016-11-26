c19922952c73173c43dcb9efbe7d2a83739d6a53
Maciej Chołuj <cholujm@student.mini.pw.edu.pl>
2016-11-26 16:59:01 +0100

    ActionCenter controls working, methods implemented in ACFragment, displaying in logs for now

--- app/src/main/java/com/nibiru/evil_ap/fragments/ACFragment.java
+++ app/src/main/java/com/nibiru/evil_ap/fragments/ACFragment.java
@@ -4,29 +4,19 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import com.nibiru.evil_ap.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ACFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ACFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ACFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class ACFragment extends Fragment implements View.OnClickListener,CompoundButton.OnCheckedChangeListener{
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
@@ -34,38 +24,32 @@ public class ACFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ACFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ACFragment newInstance(String param1, String param2) {
-        ACFragment fragment = new ACFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ac, container, false);
+        View v = inflater.inflate(R.layout.fragment_ac, container, false);
+
+        Button b = (Button) v.findViewById(R.id.button_injectHTML);
+        Button bb = (Button) v.findViewById(R.id.button_replaceImages);
+        b.setOnClickListener(this);
+        bb.setOnClickListener(this);
+        Switch switchRedirectHTML = (Switch) v.findViewById(R.id.switch1);
+        Switch switchInjectHTML = (Switch) v.findViewById(R.id.switch2);
+        Switch switchSSLStrip = (Switch) v.findViewById(R.id.switch3);
+        Switch switchReplacImages = (Switch) v.findViewById(R.id.switch4);
+        switchRedirectHTML.setOnCheckedChangeListener(this);
+        switchInjectHTML.setOnCheckedChangeListener(this);
+        switchSSLStrip.setOnCheckedChangeListener(this);
+        switchReplacImages.setOnCheckedChangeListener(this);
+        return v;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -92,6 +76,36 @@ public class ACFragment extends Fragment {
         mListener = null;
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_injectHTML:
+                Log.e("E!","inject");
+                Toast.makeText(getContext(),"inject",Toast.LENGTH_SHORT);
+                break;
+            case R.id.button_replaceImages:
+                Log.e("E!","images");
+                Toast.makeText(getContext(),"images",Toast.LENGTH_SHORT);
+                break;
+        }
+    }
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        switch (buttonView.getId()) {
+            case R.id.switch1:
+                Log.e("Switch - ","redirect "+isChecked);
+                break;
+            case R.id.switch2:
+                Log.e("Switch - ","inject "+isChecked);
+                break;
+            case R.id.switch3:
+                Log.e("Switch - ","strip "+isChecked);
+                break;
+            case R.id.switch4:
+                Log.e("Switch - ","images "+isChecked);
+                break;
+        }
+    }
     /**
      * This interface must be implemented by activities that contain this
      * fragment to allow an interaction in this fragment to be communicated
